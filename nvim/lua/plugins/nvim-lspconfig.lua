e926699c12b9991b73d223b465912b13cd482cab
4jamesccraven <4jamesccraven@gmail.com>
2025-01-25 20:15:39 -0500

    update nvim plugins

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,10 +1,10 @@
 return {
     "neovim/nvim-lspconfig",
     config = function()
-        require('lspconfig').clangd.setup{
-            cmd = {"clangd"},
-            root_dir = require('lspconfig/util').root_pattern('build/compile_commands/json', '.git'),
-        }
+        --require('lspconfig').clangd.setup{
+        --    cmd = {"clangd"},
+        --    root_dir = require('lspconfig/util').root_pattern('build/compile_commands/json', '.git'),
+        --}
         require('lspconfig').pyright.setup{}
         require('lspconfig').rust_analyzer.setup{}
     end,
